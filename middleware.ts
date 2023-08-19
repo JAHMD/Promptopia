756028b5efe18da80b3eba7f2c1763e6d574e6f3
Ahmed Wael <awael3977@gmail.com>
2023-08-19 21:21:39 +0300

    Update middleware.ts file

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-	const token = request.cookies.get("next-auth.session-token")?.value;
+	const token = request.cookies.get("__Secure-next-auth.session-token")?.value;
 	const isProtected = request.nextUrl.pathname.endsWith("/profile");
 	if (isProtected && !token) {
 		return NextResponse.redirect(new URL("/", request.url));
